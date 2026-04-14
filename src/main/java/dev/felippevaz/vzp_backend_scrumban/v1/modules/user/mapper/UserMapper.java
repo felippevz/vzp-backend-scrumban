6065651476d6e5e9dcd87ed63c80b3a674f41553
Felippe Vaz <felippe.vz@gmail.com>
2026-04-14 16:10:03 -0300

    feat: add updateFromDTO method to UserMapper for user updates from DTO

--- src/main/java/dev/felippevaz/vzp_backend_scrumban/v1/modules/user/mapper/UserMapper.java
+++ src/main/java/dev/felippevaz/vzp_backend_scrumban/v1/modules/user/mapper/UserMapper.java
@@ -1,12 +1,22 @@
 package dev.felippevaz.vzp_backend_scrumban.v1.modules.user.mapper;
 
 import dev.felippevaz.vzp_backend_scrumban.v1.modules.user.domain.User;
+import dev.felippevaz.vzp_backend_scrumban.v1.modules.user.dto.request.UserUpdateDTO;
 import dev.felippevaz.vzp_backend_scrumban.v1.modules.user.dto.response.UserResponseDTO;
-import org.mapstruct.Mapper;
-import org.mapstruct.ReportingPolicy;
+import org.mapstruct.*;
 
 @Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.ERROR)
 public interface UserMapper {
+
+    @BeanMapping(nullValuePropertyMappingStrategy = NullValuePropertyMappingStrategy.IGNORE)
+    @Mapping(target = "id", ignore = true)
+    @Mapping(target = "createdAt", ignore = true)
+    @Mapping(target = "updatedAt",  ignore = true)
+    @Mapping(target = "status",  ignore = true)
+    @Mapping(target = "authorities" ,  ignore = true)
+    @Mapping(target = "role",   ignore = true)
+    @Mapping(target = "password",   ignore = true)
+    void updateFromDTO(UserUpdateDTO userUpdateDTO, @MappingTarget User user);
 
     UserResponseDTO toResponseDTO(User user);
 }
