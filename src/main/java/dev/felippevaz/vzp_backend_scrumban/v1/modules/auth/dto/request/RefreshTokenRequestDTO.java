4f19c0b76275866e5b78374334b7e49bcb84c5b4
Felippe Vaz <felippe.vz@gmail.com>
2026-04-14 14:46:50 -0300

    refactor: remove unused imports from RefreshTokenRequestDTO

--- src/main/java/dev/felippevaz/vzp_backend_scrumban/v1/modules/auth/dto/request/RefreshTokenRequestDTO.java
+++ src/main/java/dev/felippevaz/vzp_backend_scrumban/v1/modules/auth/dto/request/RefreshTokenRequestDTO.java
@@ -1,11 +1,8 @@
 package dev.felippevaz.vzp_backend_scrumban.v1.modules.auth.dto.request;
 
 import jakarta.validation.constraints.NotBlank;
-import lombok.Getter;
-
 
 public record RefreshTokenRequestDTO(
-
 
         @NotBlank
         String refreshToken
