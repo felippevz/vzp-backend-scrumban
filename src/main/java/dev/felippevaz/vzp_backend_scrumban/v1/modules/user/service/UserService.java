0c3ea6a44abbc85333b89b0215e7a2c28e80f757
Felippe Vaz <felippe.vz@gmail.com>
2026-04-14 14:49:57 -0300

    refactor: update UserService to use UserUpdateDTO and improve error handling

--- src/main/java/dev/felippevaz/vzp_backend_scrumban/v1/modules/user/service/UserService.java
+++ src/main/java/dev/felippevaz/vzp_backend_scrumban/v1/modules/user/service/UserService.java
@@ -1,59 +1,84 @@
 package dev.felippevaz.vzp_backend_scrumban.v1.modules.user.service;
 
+import dev.felippevaz.vzp_backend_scrumban.v1.commons.domain.ErrorData;
+import dev.felippevaz.vzp_backend_scrumban.v1.commons.domain.SecurityLoggerEvent;
+import dev.felippevaz.vzp_backend_scrumban.v1.commons.exceptions.RequestException;
+import dev.felippevaz.vzp_backend_scrumban.v1.commons.handler.SecurityAuditHandler;
 import dev.felippevaz.vzp_backend_scrumban.v1.modules.user.domain.User;
-import dev.felippevaz.vzp_backend_scrumban.v1.modules.user.dto.request.UserRequestDTO;
+import dev.felippevaz.vzp_backend_scrumban.v1.modules.user.dto.request.UserUpdateDTO;
 import dev.felippevaz.vzp_backend_scrumban.v1.modules.user.dto.response.UserResponseDTO;
 import dev.felippevaz.vzp_backend_scrumban.v1.modules.user.mapper.UserMapper;
 import dev.felippevaz.vzp_backend_scrumban.v1.modules.user.repository.UserRepository;
-import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
+import org.springframework.beans.BeanUtils;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
 
 @Service
 public class UserService {
 
     private final UserRepository userRepository;
     private final UserMapper userMapper;
+    private final SecurityAuditHandler securityAuditHandler;
 
-    public UserService(UserRepository userRepository, UserMapper userMapper) {
+    public UserService(UserRepository userRepository, UserMapper userMapper, SecurityAuditHandler securityAuditHandler) {
         this.userRepository = userRepository;
         this.userMapper = userMapper;
+        this.securityAuditHandler = securityAuditHandler;
     }
 
+    @PreAuthorize("hasRole('ADMIN') ")
     public Page<UserResponseDTO> findAll(Pageable pageable) {
         return userRepository.findAll(pageable).map(userMapper::toResponseDTO);
     }
 
+    @PreAuthorize("hasRole('ADMIN') or @securityService.isOwner(#id, principal)")
     public UserResponseDTO read(Long id) {
         return userMapper.toResponseDTO(getUser(id));
     }
 
     @Transactional
-    public UserResponseDTO update(Long id, UserRequestDTO userRequestDTO) {
+    @PreAuthorize("hasRole('ADMIN') or @securityService.isOwner(#id, principal)")
+    public UserResponseDTO update(Long id, UserUpdateDTO userUpdateDTO) {
 
         User user = getUser(id);
-        this.userMapper.updateEntityFromDTO(userRequestDTO, user);
+
+        if(!userUpdateDTO.email().equals(user.getEmail()) && userRepository.existsByEmail(userUpdateDTO.email())) {
+            throw new RequestException(ErrorData.INVALID_UPDATE_EMAIL);
+        }
+
+        BeanUtils.copyProperties(userUpdateDTO, user);
 
         return userMapper.toResponseDTO(user);
     }
 
+    @PreAuthorize("hasRole('ADMIN') or @securityService.isOwner(#id, principal)")
     public void delete(Long id) {
         userRepository.deleteById(id);
     }
 
     private User getUser(Long id) {
-        return userRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("User not found"));
-        //todo: treat error later
+        return userRepository.findById(id)
+                    .orElseThrow(() -> new RequestException(ErrorData.USER_NOT_FOUND)
+                );
     }
 
     public User getUser(String username) {
-        return userRepository.findByUsername(username).orElseThrow(() -> new UsernameNotFoundException("username not found"));
-        //todo: treat error later / default error message
+        return userRepository.findByUsername(username)
+                .orElseThrow(() -> {
+                    securityAuditHandler.log(SecurityLoggerEvent.LOGIN_FAILED_USER_NOT_FOUND, username);
+                    return new RequestException(ErrorData.BAD_CREDENTIALS);
+                });
+    }
+
+    public boolean exists(String username, String email) {
+        return userRepository.existsByUsername(username) || userRepository.existsByEmail(email);
+    }
+
+    public User create(User user) {
+        return userRepository.save(user);
     }
 }
