5ffc46b096c8cdb6fdd6463ffccf2873a6ce64a8
Felippe Vaz <felippe.vz@gmail.com>
2026-04-14 14:39:50 -0300

    feat: add SecurityAuditHandler for logging security events

---
+++ src/main/java/dev/felippevaz/vzp_backend_scrumban/v1/commons/handler/SecurityAuditHandler.java
@@ -0,0 +1,24 @@
+package dev.felippevaz.vzp_backend_scrumban.v1.commons.handler;
+
+import dev.felippevaz.vzp_backend_scrumban.v1.commons.domain.SecurityLoggerEvent;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.Objects;
+
+@Slf4j
+@Component
+public class SecurityAuditHandler {
+
+    public void log(SecurityLoggerEvent event, Object... args) {
+        String formattedMessage = String.format("[%s] %s", event.getCode(), event.getMessageTemplate());
+
+        switch (event.getLevel()) {
+            case INFO -> log.info(formattedMessage, args);
+            case WARN -> log.warn(formattedMessage, args);
+            case ERROR -> log.error(formattedMessage, args);
+            case DEBUG -> log.debug(formattedMessage, args);
+            default -> log.trace(formattedMessage, args);
+        }
+    }
+}
