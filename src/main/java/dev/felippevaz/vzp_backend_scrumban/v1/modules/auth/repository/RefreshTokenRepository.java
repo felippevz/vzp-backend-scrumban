cb45d9640f0c7a710baec995621de65e8b995b98
Felippe Vaz <felippe.vz@gmail.com>
2026-04-14 14:46:25 -0300

    refactor: remove unused User import from RefreshTokenRepository

--- src/main/java/dev/felippevaz/vzp_backend_scrumban/v1/modules/auth/repository/RefreshTokenRepository.java
+++ src/main/java/dev/felippevaz/vzp_backend_scrumban/v1/modules/auth/repository/RefreshTokenRepository.java
@@ -1,7 +1,6 @@
 package dev.felippevaz.vzp_backend_scrumban.v1.modules.auth.repository;
 
 import dev.felippevaz.vzp_backend_scrumban.v1.modules.auth.domain.RefreshToken;
-import dev.felippevaz.vzp_backend_scrumban.v1.modules.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +10,4 @@ import java.util.Optional;
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
 
     Optional<RefreshToken> findByToken(String token);
-
-    void deleteByUser(User user);
 }
