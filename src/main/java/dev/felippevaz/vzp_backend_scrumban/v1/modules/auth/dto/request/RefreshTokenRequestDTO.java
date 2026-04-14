400c415d5116d1f26117246e0064556efe6d68b5
Felippe Vaz <felippe.vz@gmail.com>
2026-04-14 14:43:49 -0300

    refactor: convert RefreshTokenRequestDTO to a record for improved immutability

--- src/main/java/dev/felippevaz/vzp_backend_scrumban/v1/modules/auth/dto/request/RefreshTokenRequestDTO.java
+++ src/main/java/dev/felippevaz/vzp_backend_scrumban/v1/modules/auth/dto/request/RefreshTokenRequestDTO.java
@@ -3,9 +3,10 @@ package dev.felippevaz.vzp_backend_scrumban.v1.modules.auth.dto.request;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Getter;
 
-@Getter
-public class RefreshTokenRequestDTO {
 
-    @NotBlank
-    private String refreshToken;
-}
+public record RefreshTokenRequestDTO(
+
+
+        @NotBlank
+        String refreshToken
+) {}
