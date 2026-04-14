f894e17f3e7b4576cf034e777af0b8dcf6d938b6
Felippe Vaz <felippe.vz@gmail.com>
2026-04-14 14:40:21 -0300

    feat: enhance SecurityFilter to handle invalid tokens with exception resolution

--- src/main/java/dev/felippevaz/vzp_backend_scrumban/v1/modules/auth/filter/SecurityFilter.java
+++ src/main/java/dev/felippevaz/vzp_backend_scrumban/v1/modules/auth/filter/SecurityFilter.java
@@ -1,5 +1,7 @@
 package dev.felippevaz.vzp_backend_scrumban.v1.modules.auth.filter;
 
+import dev.felippevaz.vzp_backend_scrumban.v1.commons.domain.ErrorData;
+import dev.felippevaz.vzp_backend_scrumban.v1.commons.exceptions.RequestException;
 import dev.felippevaz.vzp_backend_scrumban.v1.modules.auth.service.TokenService;
 import dev.felippevaz.vzp_backend_scrumban.v1.modules.auth.domain.JWTUserDATA;
 import jakarta.servlet.FilterChain;
@@ -7,11 +9,14 @@ import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import org.apache.logging.log4j.util.Strings;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationServiceException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import java.io.IOException;
 import java.util.List;
@@ -21,24 +26,29 @@ import java.util.Optional;
 public class SecurityFilter extends OncePerRequestFilter {
 
     private final TokenService tokenService;
+    private final HandlerExceptionResolver handlerExceptionResolver;
 
-    public SecurityFilter(TokenService tokenService) {
+    public SecurityFilter(TokenService tokenService, HandlerExceptionResolver handlerExceptionResolver) {
         this.tokenService = tokenService;
+        this.handlerExceptionResolver = handlerExceptionResolver;
     }
 
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        try {
 
-        String token = extractToken(request);
+            String token = extractToken(request);
 
-        if (token != null) {
+            if (token == null) {
+                filterChain.doFilter(request, response);
+                return;
+            }
 
             Optional<JWTUserDATA> userData = tokenService.validateToken(token);
 
             if (userData.isPresent()) {
 
                 JWTUserDATA user = userData.get();
-
                 var authorities = List.of(new SimpleGrantedAuthority("ROLE_" + user.role().getName()));
 
                 UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
@@ -48,10 +58,16 @@ public class SecurityFilter extends OncePerRequestFilter {
                 );
 
                 SecurityContextHolder.getContext().setAuthentication(authentication);
-            }
-        }
 
-        filterChain.doFilter(request, response);
+                filterChain.doFilter(request, response);
+
+            } else {
+                throw new RequestException(ErrorData.INVALID_TOKEN);
+            }
+
+        } catch (Exception exception) {
+            handlerExceptionResolver.resolveException(request, response, null, exception);
+        }
     }
 
     private String extractToken(HttpServletRequest request) {
