36271db5b703472cf30be96783f9e5ad48c5a9db
Felippe Vaz <felippe.vz@gmail.com>
2026-04-14 16:09:49 -0300

    feat: add deleteByUser method to RefreshTokenRepository for user-specific token removal

--- src/main/java/dev/felippevaz/vzp_backend_scrumban/v1/modules/auth/repository/RefreshTokenRepository.java
+++ src/main/java/dev/felippevaz/vzp_backend_scrumban/v1/modules/auth/repository/RefreshTokenRepository.java
@@ -1,6 +1,7 @@
 package dev.felippevaz.vzp_backend_scrumban.v1.modules.auth.repository;
 
 import dev.felippevaz.vzp_backend_scrumban.v1.modules.auth.domain.RefreshToken;
+import dev.felippevaz.vzp_backend_scrumban.v1.modules.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,4 +11,6 @@ import java.util.Optional;
 public interface RefreshTokenRepository extends JpaRepository<RefreshToken, Long> {
 
     Optional<RefreshToken> findByToken(String token);
+
+    void deleteByUser(User user);
 }
